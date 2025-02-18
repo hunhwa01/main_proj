1dd38e502ae0511c18b59f6abb4ee5743f145535
hunhwa01 <seungbin018838@gmail.com>
2025-02-18 13:47:03 +0900

    지도 수정

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -21,33 +21,28 @@ const Map = () => {
         return;
       }
 
-      // ✅ 출발지 (첫 번째 값)
       const startLocation = data[0];
       const startPosition = new window.Tmapv2.LatLng(startLocation.latitude, startLocation.longitude);
 
-      // ✅ 목적지 (마지막 값)
       const endLocation = data[data.length - 1];
       const endPosition = new window.Tmapv2.LatLng(endLocation.latitude, endLocation.longitude);
 
-      // ✅ 지도 생성 (출발지를 중심으로)
       const newMap = new window.Tmapv2.Map(mapRef.current, {
-        center: startPosition, // 지도 중심
+        center: startPosition,
         width: "100%",
-        height: "500px",
+        height: "100%",
         zoom: 16,
       });
 
       setMap(newMap);
       console.log("🗺️ 지도 객체 생성 완료:", newMap);
 
-      // ✅ 출발지 마커 추가
       const startMarker = new window.Tmapv2.Marker({
         position: startPosition,
         map: newMap,
         label: "출발지",
       });
 
-      // ✅ 목적지 마커 추가
       const endMarker = new window.Tmapv2.Marker({
         position: endPosition,
         map: newMap,
@@ -57,7 +52,6 @@ const Map = () => {
       console.log("📍 출발지 마커 추가 완료:", startMarker);
       console.log("📍 목적지 마커 추가 완료:", endMarker);
 
-      // ✅ 보행자 경로 요청
       drawPedestrianRoute(startLocation, endLocation, newMap);
     } catch (error) {
       console.error("🚨 주소 데이터를 불러오는데 실패했습니다:", error);
@@ -65,6 +59,7 @@ const Map = () => {
   };
 
   const drawPedestrianRoute = async (start, end, mapInstance) => {
+    
     try {
       const response = await fetch(
         `http://localhost:8000/proxy/tmap-route?start=${start.longitude},${start.latitude}&goal=${end.longitude},${end.latitude}`
@@ -93,9 +88,10 @@ const Map = () => {
       // ✅ 새로운 폴리라인 추가
       const newPolyline = new window.Tmapv2.Polyline({
         path: path,
-        strokeColor: "#FF0000",
-        strokeWeight: 4,
+        strokeColor: "#0000FF",
+        strokeWeight: 6,
         map: mapInstance,
+        zIndex: 1000,
       });
 
       setPolyline(newPolyline);
@@ -106,8 +102,8 @@ const Map = () => {
   };
 
   return (
-    <div>
-      <div id="map" ref={mapRef} style={{ width: "100%", height: "100%" }} />
+    <div style={{ width: "100%", height: "100%" }}>
+      <div id="map" ref={mapRef} style={{ width: "100%", height: "100%", borderRadius: "20px" }} />
     </div>
   );
 };
